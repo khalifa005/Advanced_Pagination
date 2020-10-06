29c2b7c5b5513ea071a64ceac6b6fd4c04b7c31c
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-06 23:56:08 +0200

    Configure startup class for swwagert api and ef core

--- AdvancedPagination/Startup.cs
+++ AdvancedPagination/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace AdvancedPagination
 {
@@ -25,6 +27,24 @@ namespace AdvancedPagination
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\AdvancedPagination.WebApi.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AdvancedPagination.WebApi",
+                });
+            });
+            #endregion
+
             services.AddControllers();
         }
 
@@ -36,6 +56,17 @@ namespace AdvancedPagination
                 app.UseDeveloperExceptionPage();
             }
 
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdvancedPagination.WebApi");
+            });
+            #endregion
             app.UseHttpsRedirection();
 
             app.UseRouting();

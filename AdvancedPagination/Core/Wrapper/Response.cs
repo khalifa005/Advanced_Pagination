dfef9c075b55ae204e280fd88e9675de7500efc2
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-10 15:00:18 +0200

    Add response wrappers

---
+++ AdvancedPagination/Core/Wrapper/Response.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace AdvancedPagination.Core.Wrapper
+{
+    /// <summary>
+    /// use this wrapper when return happen in the controller direct 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Response<T>
+    {
+        public Response()
+        {
+            
+        }
+
+        public Response(T data)
+        {
+            Data = data;
+            Succeeded = true;
+            Errors = null;
+            Message = string.Empty;
+            StatusCode = StatusCodes.Status200OK;
+        }
+
+        public T Data { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Succeeded { get; set; }
+
+        public int StatusCode { get; set; }
+    }
+}

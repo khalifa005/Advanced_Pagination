1d2e8e90efe1f45b0b29f7ff32f12449b2ccdd06
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-10-10 15:01:32 +0200

    add uri service and common configuration

---
+++ AdvancedPagination/Core/Util/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedPagination.Core.Util
+{
+    public static class Config
+    {
+        public const string PageNumber = "pageNumber";
+        public const string PageSize = "pageSize";
+        
+        public const int DefaultPageSize = 10;
+        public const int DefaultPageNumber = 1;
+
+
+    }
+}
---
+++ AdvancedPagination/Core/Util/UriService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvancedPagination.Core.Filter;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace AdvancedPagination.Core.Util
+{
+    public interface IUriService
+    {
+        public Uri GetPageUri(PaginationFilter filter, string route);
+    }
+    public class UriService : IUriService
+    {
+        private readonly string _baseUrl;
+
+        public UriService(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+        public Uri GetPageUri(PaginationFilter filter, string route)
+        {
+            var endPointUri = string.Concat(_baseUrl, route);
+            var modifiedUri = QueryHelpers.AddQueryString(endPointUri.ToString(), Config.PageNumber, filter.PageNumber.ToString());
+            modifiedUri = QueryHelpers.AddQueryString(modifiedUri, Config.PageSize, filter.PageSize.ToString());
+            return new Uri(modifiedUri);
+        }
+    }
+}
